74994875a18d73beabd7c1620d0466fe2547776a
Douzon <thibault.douzon@esker.corp>
2020-01-15 18:36:38 +0100

    hyperoptimisation of parameters & remove useless stuff

--- robust_parser/main.py
+++ robust_parser/main.py
@@ -27,308 +27,101 @@ def showPlot(points):
     plt.plot(points)
 
 
-teacher_forcing_ratio = 0.8
-
-
-def teacher_forcing(decoder_hidden, encoder_output, target_tensor, decoder, criterion):
-    loss = 0
-    target_length = target_tensor.size(0)
-    batch_size = target_tensor.size(1)
-
-    decoder_input = torch.cat(
-        (
-            torch.full(
-                size=(1, batch_size),
-                fill_value=data.vocabulary[data.__BEG__],
-                dtype=torch.long,
-            ),
-            target_tensor[:-1, ...],
-        ),
-        dim=0,
-    ).refine_names("O", "B")
-    decoder_output, decoder_hidden = decoder(decoder_input, decoder_hidden)
-
-    loss += criterion(
-        decoder_output.align_to("B", "V", "O").rename(None),
-        target_tensor.align_to("B", "O").rename(None),
-    )
-
-    return loss
-
-
-def greedy_decode(decoder, decoder_input, decoder_hidden, criterion, target_tensor):
-    target_length = target_tensor.size(0)
-    pred_seq = []
-    loss = 0
-    for seq_pos in range(target_length):
-        decoder_output, decoder_hidden = decoder(decoder_input, decoder_hidden)
-
-        topv, topi = decoder_output.rename(None).topk(1, dim=-1)
-        topi = topi.squeeze(-1).refine_names("O", "B")
-        decoder_input = topi.detach()  # detach from history as input
-
-        loss += criterion(
-            decoder_output.align_to("B", "V", "O").rename(None),
-            target_tensor.align_to("B", "O").rename(None)[:, seq_pos].unsqueeze(-1),
-        )
-        pred_seq.append(decoder_input.squeeze().numpy())
-
-    return loss, pred_seq
-
-
-def greedy_decode_training(
-    decoder_hidden, encoder_output, target_tensor, decoder, criterion
-):
-    loss = 0
-    target_length = target_tensor.size(0)
-    batch_size = target_tensor.size(1)
-
-    decoder_input_b = torch.full(
-        size=(1, batch_size), fill_value=data.vocabulary[data.__BEG__], dtype=torch.long
-    )
-    decoder_hidden_b = decoder_hidden
-
-    loss, _ = greedy_decode(decoder, decoder_input_b, decoder_hidden_b, criterion, target_tensor)
-
-    return loss
-
-
-def L2_regularize(model_l, l2_penalty=10**-4):
-    loss_reg = 0
-    crit = nn.MSELoss()
-    for m in model_l:
-        for p in m.parameters():
-            loss_reg += crit(p, torch.zeros_like(p))
-    return l2_penalty * loss_reg
-
-
-def train(
-    input_tensor,
-    target_tensor,
-    encoder,
-    decoder,
-    encoder_optimizer,
-    decoder_optimizer,
-    criterion,
-    max_length=20
-):
-
-    encoder_optimizer.zero_grad()
-    decoder_optimizer.zero_grad()
-
-    loss = 0
-
-    encoder_output, encoder_hidden = encoder(input_tensor)
-
-    # encoder_outputs = torch.cat(
-    #     (
-    #         encoder_outputs,
-    #         torch.zeros(
-    #             (max_length - input_length, batch_size, encoder_outputs.size(2))
-    #         ),
-    #     ),
-    #     dim=0,
-    # )
-
-    decoder_hidden = encoder_hidden
-
-    use_teacher_forcing = random.random() < teacher_forcing_ratio
-
-    if use_teacher_forcing:
-        loss = teacher_forcing(
-            decoder_hidden, encoder_output, target_tensor, decoder, criterion
-        )
-
-    else:
-        loss = greedy_decode_training(
-            decoder_hidden, encoder_output, target_tensor, decoder, criterion
-        )
-
-    loss += L2_regularize([encoder, decoder])
-
-    loss.backward()
-
-    encoder_optimizer.step()
-    decoder_optimizer.step()
-
-    return loss.item()
-
-
-def train_iters(
-    encoder,
-    decoder,
-    n_epochs,
-    dataset: data.DateLoader,
-    dataset_test: data.DateLoader,
-    *,
-    eval_every=2,
-    learning_rate=0.003
-):
-    start = time.time()
-
-    encoder.train()
-    decoder.train()
-
-    gradient_clip = 10
-    if gradient_clip is not None:
-        for model in [encoder, decoder]:
-            for p in model.parameters():
-                p.register_hook(lambda grad: torch.clamp(grad, -gradient_clip, gradient_clip))
-
-    # plot_losses = []
-    print_loss_total = 0  # Reset every print_every
-    plot_loss_total = 0  # Reset every plot_every
-
-    encoder_optimizer = optim.AdamW(encoder.parameters(), lr=learning_rate)
-    decoder_optimizer = optim.AdamW(decoder.parameters(), lr=learning_rate)
-
-    criterion = nn.NLLLoss(ignore_index=data.vocabulary[data.__PAD__])
-
-    for epoch in range(n_epochs):
-        dataset.redraw_dataset()
-        for x, y in tqdm.tqdm(dataset):
-
-            loss = train(
-                x, y, encoder, decoder, encoder_optimizer, decoder_optimizer, criterion,
-            )
-            print_loss_total += loss
-            plot_loss_total += loss
-
-        print_loss_avg = print_loss_total / len(dataset)
-        print_loss_total = 0
-        print(
-            f"[{epoch + 1}] {(epoch+1)/n_epochs*100:4.2f}% ({datetime.timedelta(seconds=int(time.time()-start))!s}) | Loss={print_loss_avg:.5f}"
-        )
-
-        if (epoch + 1) % eval_every == 0:
-            evaluate(encoder, decoder, dataset_test, criterion, verbose=True)
-
-    return evaluate(encoder, decoder, dataset_test, criterion)
-    #     if iter % plot_every == 0:
-    #         plot_loss_avg = plot_loss_total / plot_every
-    #         plot_losses.append(plot_loss_avg)
-    #         plot_loss_total = 0
-
-    # showPlot(plot_losses)
-
-
-def evaluate(encoder, decoder, dataset, criterion, *, verbose=False):
-    encoder.eval()
-    decoder.eval()
-    
-    rev_vocabulary = list(data.vocabulary.keys())
-
-    with torch.no_grad():
-        errors = []
-        count_ok = 0
-        loss = 0
-        for x, y in dataset:
-            target_length = y.size(0)
-            batch_size = y.size(1)
-
-            encoder_output, encoder_hidden = encoder(x)
-            decoder_hidden = encoder_hidden
-            decoder_input_b = torch.full(
-                size=(1, batch_size),
-                fill_value=data.vocabulary[data.__BEG__],
-                dtype=torch.long,
-            )
-            decoder_hidden_b = decoder_hidden
-
-            local_loss, pred_seq = greedy_decode(decoder, decoder_input_b, decoder_hidden_b, criterion, y)
-
-            loss += local_loss
-            
-            if any(p != yy.item() for p, yy in zip(pred_seq, y)):
-                if verbose:
-                    errors.append((f"PRD : {''.join(rev_vocabulary[i] for i in pred_seq)}",
-                                   f"TGT : {''.join(rev_vocabulary[i.item()] for i in y)}",
-                                   f"RAW : {''.join(rev_vocabulary[i.item()] for i in x)}",
-                                   "\n"))
-            else:
-                count_ok += 1
-        print(*("\n".join(i) for i in random.choices(errors, k=min(10, len(errors)))))
-        print(f"[ACCURACY]: {count_ok / len(dataset)}")
-        print(f"[LOSS]: {loss / len(dataset)}")
-        
-        return loss.item()
-
-     
 def train_eval(parameters):
-    learning_rate = parameters["learning_rate"]
-    hidden_size = parameters["hidden_size"]
-    n_mogrify = parameters["n_mogrify"]
-    
     data.set_seed()
+    encoder_cls = parameters.get("encoder_cls", model.EncoderRNN)
+    decoder_cls = parameters.get("decoder_cls", model.DecoderRNN)
 
-    train_data_size = 5 * 10 ** 3
-    test_data_size = 10 ** 3
+    encoder_prm = parameters.get("encoder_prm", {})
+    decoder_prm = parameters.get("decoder_prm", {})
 
-    batch_size = 64
+    hidden_size = parameters.get("hidden_size", 128)
 
-    dataset_train = data.get_date_dataloader(
-        data.DateDataset(train_data_size), batch_size
-    )
-    dataset_test = data.get_date_dataloader(data.DateDataset(test_data_size), 1)
-
-    encoder, decoder = (à
-        mogrifier.EncoderMogrifierRNN(len(data.vocabulary), hidden_size, n_mogrify),
-        # model.EncoderRNN(len(data.vocabulary), hidden_size),
-        mogrifier.DecoderMogrifierRNN(hidden_size, len(data.vocabulary), n_mogrify),
-        # model.DecoderRNN(hidden_size, len(data.vocabulary)),
+    m = model.EncoderDecoderModel(
+        encoder_cls=encoder_cls,
+        decoder_cls=decoder_cls,
+        encoder_prm=encoder_prm,
+        decoder_prm=decoder_prm,
+        hidden_size=hidden_size,
     )
 
-    return train_iters(encoder, decoder, 10, dataset_train, dataset_test, eval_every=20, learning_rate=learning_rate)
-    
+    batch_size = parameters.get("batch_size", 32)
 
+    dataset_train = data.get_date_dataloader(data.DateDataset(10 ** 3), batch_size)
+    dataset_test = data.get_date_dataloader(data.DateDataset(10 ** 4), batch_size)
 
-def main():
-    data.set_seed()
+    learning_rate = parameters.get("learning_rate", 10 ** -3)
+    n_epochs = parameters.get("n_epochs", 30)
+    teacher_forcing_ratio = parameters.get("teacher_forcing_ratio", 0.5)
+    l2_penalty = parameters.get("l2_penalty", None)
 
-    train_data_size = 5 * 10 ** 3
-    test_data_size = 10 ** 3
+    optimizer = parameters.get("optimizer", "Adam")
 
-    batch_size = 64
-    hidden_size = 256
-
-    
-
-    dataset_train = data.get_date_dataloader(
-        data.DateDataset(train_data_size), batch_size
-    )
-    dataset_test = data.get_date_dataloader(data.DateDataset(test_data_size), 1)
-
-    encoder, decoder = (
-        mogrifier.EncoderMogrifierRNN(len(data.vocabulary), hidden_size, 6),
-        # model.EncoderRNN(len(data.vocabulary), hidden_size),
-        mogrifier.DecoderMogrifierRNN(hidden_size, len(data.vocabulary), 6),
-        # model.DecoderRNN(hidden_size, len(data.vocabulary)),
+    m.fit(
+        dataset_train,
+        dataset_test,
+        learning_rate=learning_rate,
+        n_epochs=n_epochs,
+        teacher_forcing_ratio=teacher_forcing_ratio,
+        l2_penalty=l2_penalty,
+        optimizer=optimizer,
     )
 
-    train_iters(encoder, decoder, 5, dataset_train, dataset_test)
+    evaluation = m.evaluate(dataset_test)
+    print(parameters, evaluation, sep='\n')
+    return evaluation["loss"]
 
-    return encoder, decoder
 
-
-if __name__ == "__main__":
-    # main()
-    # train_eval({"learning_rate": 0.001, "n_mogrify": 3, "hidden_size": 32})
-    
-    from ax.plot.contour import plot_contour
-    from ax.plot.trace import optimization_trace_single_method
+def hyper_opt():
     from ax.service.managed_loop import optimize
-    from ax.utils.notebook.plotting import render, init_notebook_plotting
+    # from ax import optimize
     
     best_parameters, values, experiment, model = optimize(
         parameters=[
-            {"name": "learning_rate", "value_type": "float", "type": "range", "bounds": [1e-6, 0.4], "log_scale": True},
-            {"name": "n_mogrify", "value_type": "int", "type": "range", "bounds": [0, 10]},
-            {"name": "hidden_size", "value_type": "int", "type": "range", "bounds": [10, 256]},
+            {
+                "name": "learning_rate",
+                "value_type": "float",
+                "type": "range",
+                "bounds": [1e-6, 0.1],
+                "log_scale": True,
+            },
+            {
+                "name": "teacher_forcing_ratio",
+                "value_type": "float",
+                "type": "range",
+                "bounds": [0.0, 1.0]
+            },
+            {
+                "name": "l2_penalty",
+                "value_type": "float",
+                "type": "range",
+                "bounds": [1e-6, 1.0],
+                "log_scale": True
+            },
+            {
+                "name": "n_mogrify",
+                "value_type": "int",
+                "type": "range",
+                "bounds": [0, 10],
+            },
+            {
+                "name": "hidden_size",
+                "value_type": "int",
+                "type": "range",
+                "bounds": [10, 512],
+            },
         ],
         evaluation_function=train_eval,
-        objective_name='loss',
+        objective_name="loss",
         minimize=True,
-        total_trials=10
+        total_trials=20,
     )
-    
-    print(best_parameters, values, experiment, model)
+    return best_parameters, values, experiment, model
+
+
+def main():
+    hyper_opt()
+
+
+if __name__ == "__main__":
+    main()
